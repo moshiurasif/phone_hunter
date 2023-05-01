82c8997b866e102850b48986242afad9805bf19d
asif2212 <asifmd2212@gamil.com>
2023-05-02 01:08:30 +0600

    show all button integration

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const loadPhoneData = async(searchText) =>{
+const loadPhoneData = async(searchText, dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayData(data.data);
+    displayData(data.data, dataLimit);
 }
 // loadPhoneData();
 
-const displayData = (data) => {
+const displayData = (data, dataLimit) => {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerText = "";
-    data = data.slice(0, 9);
+    const showAll = document.getElementById("show-all");
+    if (dataLimit && data.length > 10) {
+        data = data.slice(0, 10);
+        showAll.classList.remove("d-none");
+    } else {
+        showAll.classList.add("d-none");
+    }
     const noFound = document.getElementById("no-found");
     if (data.length === 0) {
         noFound.classList.remove("d-none");
@@ -40,13 +46,15 @@ const displayData = (data) => {
 // stop loader
 loadSpinner(false);
 }
-
-document.getElementById("search-btn").addEventListener("click", ()=>{
+const processData = dataLimit => {
     // start loader
     loadSpinner(true);
     const searchInput = document.getElementById("search-text");
     const searchText = searchInput.value;
-    loadPhoneData(searchText);
+    loadPhoneData(searchText, dataLimit);
+}
+document.getElementById("search-btn").addEventListener("click", ()=>{
+    processData(10);
 })
 
 const loadSpinner = isLoading =>{
@@ -57,3 +65,6 @@ const loadSpinner = isLoading =>{
         loader.classList.add("d-none");
     }
 }
+document.getElementById("btn-show-all").addEventListener("click", ()=>{
+    processData();
+})
